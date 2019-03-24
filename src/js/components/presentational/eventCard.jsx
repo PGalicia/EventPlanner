a8255a18fb7061a726a7b5cbc5a4b847cf1049b3
patrick <patrickgalicia@patricks-MacBook-Pro.local>
2019-03-24 11:49:32 -0700

    Style full event card

--- src/js/components/presentational/eventCard.jsx
+++ src/js/components/presentational/eventCard.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './../../../scss/eventCard.scss';
+import { connect } from "react-redux";
 
-const EventCard = () => {
-    return (
-        <div className="event-card-container">
-            <div className="date-container">
-                <h3 className="date">10</h3>
-                <h3 className="month">FEB</h3>
-                <h4 className="day-year">Wednesday 2019</h4>
-            </div>
-            <div className="event-info-container">
-                <h2>EventCard Component</h2>
-                <h4>Location</h4>
-                <h4>Time</h4>
-                <h6>See more...</h6>
-            </div>
-        </div>
-    );
+const mapStateToProps = state => {
+    return state;
 }
 
-export default EventCard;
+const mapDispatchToProps = dispatch => {
+    return {};
+}
+
+class EventCard extends Component {
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return (
+            <div className="event-card-container">
+                <div className="date-container">
+                    <h3 className="date">10</h3>
+                    <h3 className="month">FEB</h3>
+                    <h4 className="day-year">Wednesday 2019</h4>
+                </div>
+                <div className="event-info-container">
+                    <h2 className="event-name">Game Night</h2>
+                    <h4 className="event-location">
+                        <span className="logo" id="location">logo</span>
+                        123 North Avenue Los Angeles, CA 90041
+                    </h4>
+                    <h4 className="event-time">
+                        <span className="logo" id="time">logo</span>
+                        10:00 AM
+                    </h4>
+                    <h6 className="see-more-button">See more...</h6>
+                    <div className="event-card-action-button-container">
+                        <button id="expand-button">m</button>
+                        <button id="edit-button">e</button>
+                        <button id="delete-button">d</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(EventCard);
