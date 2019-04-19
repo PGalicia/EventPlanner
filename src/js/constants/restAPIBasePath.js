0bcc8b5672f5cd68ad85dda64db6cc22712efbe7
Patrick Galicia <galicia.patrick@gmail.com>
2019-04-19 14:28:03 -0700

    Updated REST_API_BASE_PATH

--- src/js/constants/restAPIBasePath.js
+++ src/js/constants/restAPIBasePath.js
@@ -3,5 +3,8 @@
     Description: Base link for the REST API
 */
 
+// Local REST API
+// export const REST_API_BASE_PATH = "http://localhost:3001";
 
-export const REST_API_BASE_PATH = "http://localhost:3001";
+// Cloud API
+export const REST_API_BASE_PATH = "https://fathomless-basin-43718.herokuapp.com";
