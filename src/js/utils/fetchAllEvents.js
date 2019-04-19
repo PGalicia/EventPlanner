90d2084c3a53656ebbdd20ba2caf8fb37fc23ff5
Patrick Galicia <galicia.patrick@gmail.com>
2019-04-19 14:00:41 -0700

    Restore project to connect to REST

--- src/js/utils/fetchAllEvents.js
+++ src/js/utils/fetchAllEvents.js
@@ -8,15 +8,11 @@
 */
 
 import { REST_API_BASE_PATH } from "./../constants/restAPIBasePath.js" // Constants
-import events from "../_database/events.js"; // Pseudo Database
 
 export function fetchAllEvents(id = "") {
 
-    // Fetch 
-    return id === "" ? Promise.resolve(events) : Promise.resolve(events.find(event => event.rowid == id))
-
-    // return fetch(`${REST_API_BASE_PATH}/events/${id}`)
-    //     .then(res => res.json())
-    //     .then(events => events)
+    return fetch(`${REST_API_BASE_PATH}/events/${id}`)
+        .then(res => res.json())
+        .then(events => events)
 
 }
--- src/js/utils/fetchAllItems.js
+++ src/js/utils/fetchAllItems.js
@@ -8,14 +8,11 @@
 */
 
 import { REST_API_BASE_PATH } from "./../constants/restAPIBasePath.js" // Constants
-import items from "./../_database/items.js"; // Pseudo Database
 
 export function fetchAllItems() {
 
-    return Promise.resolve(items);
-
-    // return fetch(`${REST_API_BASE_PATH}/items/`)
-    //     .then(res => res.json())
-    //     .then(items => items)
+    return fetch(`${REST_API_BASE_PATH}/items/`)
+        .then(res => res.json())
+        .then(items => items)
 
 }
